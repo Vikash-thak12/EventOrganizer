baba801fce435e59363801b65cd1aa5bf0c80941
Vikash Thakur <thakvikash622@gmail.com>
2025-11-25 14:44:49 +0530

    Initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Header = () => {
+    return (
+        <>
+            <nav className='flex items-center justify-between px-10 py-5 w-full bg-background/10 z-10'>
+                <div>
+                    <Link href={"/"} className='cursor-pointer'>
+                        <Image src={"/spott.png"} alt='Logo Image' height={100} width={100} />
+                        {/* Pro Badge */}
+                    </Link>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header
