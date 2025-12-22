d4c78935c5170507f9e638e808f37e9d29d6b275
Vikash Thakur <thakvikash622@gmail.com>
2025-12-22 15:08:21 +0530

    SetUp: Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "../components/Header";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 // import Header from "@/components/Header";
 
@@ -21,25 +23,31 @@ export default function RootLayout({
       <body
         className={`bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white`}
       >
-        {/* Header */}
-        <Header />  
+        <ClerkProvider>
+          <ConvexClientProvider>
+            {/* Header */}
+            <Header />
 
-        {/* note: container */}
-        <main className="min-h-screen relative mx-auto">
+            {/* note: container */}
+            <main className="min-h-screen relative mx-auto">
 
-          {/* this one is for glow  */}
-          <div className="absolute pointer-events-none inset-0 overflow-hidden -z-10">
-            <div className="absoulte top-0 left-1/4 max-sm:h-60 max-sm:w-60 h-96 w-96 bg-pink-600/30 rounded-full blur-3xl" />
-            <div className="absolute bottom-0 right-1/4 max-sm:h-60 max-sm:w-60 w-96 h-96 bg-orange-600/30 rounded-full blur-3xl" />
-          </div>
+              {/* this one is for glow  */}
+              <div className="absolute pointer-events-none inset-0 overflow-hidden -z-10">
+                <div className="absoulte top-0 left-1/4 max-sm:h-60 max-sm:w-60 h-96 w-96 bg-pink-600/30 rounded-full blur-3xl" />
+                <div className="absolute bottom-0 right-1/4 max-sm:h-60 max-sm:w-60 w-96 h-96 bg-orange-600/30 rounded-full blur-3xl" />
+              </div>
 
 
-          {/* todo: relative z-10 */}
-          <div>
-            {children}
-          </div>
-        </main>
-        {/* Footer */}
+              {/* todo: relative z-10 */}
+              <div>
+                {children}
+              </div>
+            </main>
+
+
+            {/* Footer */}
+          </ConvexClientProvider>
+        </ClerkProvider>
       </body>
     </html>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -10,6 +11,20 @@ const Header = () => {
                         <Image src={"/spott.png"} alt='Logo Image' height={100} width={100} />
                         {/* Pro Badge */}
                     </Link>
+                </div>
+
+                <div>
+                    <SignedOut>
+                        <SignInButton />
+                        <SignUpButton>
+                            <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                                Sign Up
+                            </button>
+                        </SignUpButton>
+                    </SignedOut>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
                 </div>
             </nav>
         </>
