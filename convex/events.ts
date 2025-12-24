23e7afc3980c930f9b3c6f23c36e2ac3e118a1db
Vikash Thakur <thakvikash622@gmail.com>
2025-12-24 13:46:28 +0530

    Completed: Explore page APIs

--- convex/events.ts
+++ convex/events.ts
@@ -36,15 +36,66 @@ export const getEventsByLocation = query({
             .withIndex("by_start_date")
             .filter((q) => q.gte(q.field("startDate"), now))
             .collect();
-        
-        
-        if(args.city){
-            events = events.filter((c) => c.city.toLowerCase() === args.city.toLowerCase()); 
-        }else{
+
+
+        if (args.city) {
+            events = events.filter((c) => c.city.toLowerCase() === args.city.toLowerCase());
+        } else {
             events = events.filter((s) => s.state.toLowerCase() === args.state.toLowerCase())
         }
 
-        return events.slice(0, args.limit ?? 4); 
+        return events.slice(0, args.limit ?? 4);
 
     }
 })
+
+
+export const getPopularEvents = query({
+    args: {
+        limit: v.optional(v.number()),
+    },
+    handler: async (ctx, args) => {
+        const now = Date.now();
+        const events = await ctx.db.query("events")
+            .withIndex("by_start_date")
+            .filter((q) => q.gte(q.field("startDate"), now))
+            .collect();
+
+        const popular = events.sort((a, b) => b.registrationCount - a.registrationCount).slice(0, args.limit ?? 4);
+        return popular;
+    }
+})
+
+export const getEventsByCategory = query({
+    args: {
+        category: v.string(),
+        limit: v.optional(v.number()),
+    },
+    handler: async (ctx, args) => {
+        const now = Date.now();
+        const events = await ctx.db.query("events")
+            .withIndex("by_category", (q) => q.eq("category", args.category))
+            .filter((f) => f.gte(f.field("startDate"), now))
+            .collect();
+
+        return events.slice(0, args.limit ?? 4);
+    }
+})
+
+
+export const getCategoryCounts = query({
+    handler: async (ctx) => {
+        const now = Date.now();
+        const events = await ctx.db.query("events")
+            .withIndex("by_start_date")
+            .filter((q) => q.gte(q.field("startDate"), now))
+            .collect();
+
+        const counts = {};
+        events.forEach((event) => {
+            counts[event.category] = (counts[event.category] || 0) + 1; 
+        })
+
+        return counts; 
+    }
+})  
