1810f4d00e0e3eee76dee64afca51cebda4deb89
Vikash Thakur <thakvikash622@gmail.com>
2025-12-24 13:19:36 +0530

    Created: Function to get featuredEvents

---
+++ convex/events.ts
@@ -0,0 +1,21 @@
+import { v } from "convex/values";
+import { query } from "./_generated/server";
+
+export const getFeaturedEvents = query({
+    args: {
+        limit: v.optional(v.number()),
+    },
+    handler: async (ctx, args) => {
+        const now = Date.now();
+        const events = await ctx.db.query("events")
+            .withIndex("by_start_date")
+            .filter((q) => q.gte(q.field("startDate"), now))
+            .order("desc")
+            .collect();
+
+        // sorting on the basis of most number of registered
+        const featured = events.sort((a,b) => b.registrationCount - a.registrationCount).slice(0, args.limit ?? 3); 
+        return featured; 
+
+    }
+})
