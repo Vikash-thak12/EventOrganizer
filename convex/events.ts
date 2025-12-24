cece48bf2014620cedbcf6a5d302ef0540af4d49
Vikash Thakur <thakvikash622@gmail.com>
2025-12-24 13:27:48 +0530

    Created Query to get EventsByLocation

--- convex/events.ts
+++ convex/events.ts
@@ -1,6 +1,7 @@
 import { v } from "convex/values";
 import { query } from "./_generated/server";
 
+// query to get upcoming events based on max number of participants 
 export const getFeaturedEvents = query({
     args: {
         limit: v.optional(v.number()),
@@ -14,8 +15,36 @@ export const getFeaturedEvents = query({
             .collect();
 
         // sorting on the basis of most number of registered
-        const featured = events.sort((a,b) => b.registrationCount - a.registrationCount).slice(0, args.limit ?? 3); 
-        return featured; 
+        const featured = events.sort((a, b) => b.registrationCount - a.registrationCount).slice(0, args.limit ?? 3);
+        return featured;
+
+    }
+})
+
+
+
+// get events by location like state and city 
+export const getEventsByLocation = query({
+    args: {
+        city: v.optional(v.string()),
+        state: v.optional(v.string()),
+        limit: v.optional(v.number())
+    },
+    handler: async (ctx, args) => {
+        const now = Date.now();
+        let events = await ctx.db.query("events")
+            .withIndex("by_start_date")
+            .filter((q) => q.gte(q.field("startDate"), now))
+            .collect();
+        
+        
+        if(args.city){
+            events = events.filter((c) => c.city.toLowerCase() === args.city.toLowerCase()); 
+        }else{
+            events = events.filter((s) => s.state.toLowerCase() === args.state.toLowerCase())
+        }
+
+        return events.slice(0, args.limit ?? 4); 
 
     }
 })
