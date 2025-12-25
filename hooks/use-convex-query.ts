4c4a07ab33859f91bd7aa7a4dacd1c9ff6ddcdee
Vikash Thakur <thakvikash622@gmail.com>
2025-12-25 07:10:37 +0530

    Created: Custom hooks

---
+++ hooks/use-convex-query.ts
@@ -0,0 +1,30 @@
+import { useQuery } from "convex/react"
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+
+export const useConvexQuery = (query, args = {}) => {
+    const result = useQuery(query, args); 
+
+    const[data, setData] = useState(undefined); 
+    const[isLoading, setIsLoading] = useState(true); 
+    const[error, setError] = useState(null); 
+
+
+    useEffect(() => {
+        if(result == undefined){
+            setIsLoading(true); 
+        }else{
+            try {
+                setData(result); 
+                setError(null); 
+            } catch (err) {
+                setError(err); 
+                toast.error(err.message); 
+            } finally{
+                setIsLoading(false)
+            }
+        }
+    },[result])
+
+    return { data, isLoading, error}; 
+}
