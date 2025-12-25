5299cf074711fa946ec3c8ce36270688e349c2fb
Vikash Thakur <thakvikash622@gmail.com>
2025-12-25 07:01:26 +0530

    Minor changes: for testing data

--- app/(public)/explore/page.tsx
+++ app/(public)/explore/page.tsx
@@ -1,6 +1,12 @@
+"use client"
+import { useQuery } from 'convex/react'
 import React from 'react'
+import { api } from '../../../convex/_generated/api'
 
 const ExplorePage = () => {
+  // {} is required because Convex queries with args must receive an args object
+  const data = useQuery(api.events.getPopularEvents, {});
+  console.log("Datas", data);  
   return (
     <div>
       This is explore page
