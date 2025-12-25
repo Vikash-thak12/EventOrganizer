b2bd5ce916e7b5f172a9eaeaf849ccf6f3e786d0
Vikash Thakur <thakvikash622@gmail.com>
2025-12-25 06:40:32 +0530

    Created: basic button

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
+"use client"
+import { Button } from '../../components/ui/button'
+import { ArrowLeft, Plus } from 'lucide-react'
+import { usePathname, useRouter } from 'next/navigation'
 
 const ExploreLayout = ({ children }) => {
+  const pathname = usePathname();
+  const isMainPath = pathname === "/explore";
+  const router = useRouter(); 
   return (
     <div>
-        <h1>Back to Explore page</h1>
-      {children}
+      <div className='max-w-7xl mx-auto p-5'>
+        {
+          (!isMainPath &&
+            <Button
+            onClick={() => router.push("/explore")}
+              className='cursor-pointer my-2 flex items-center justify-center'
+              variant='mine'>
+              <ArrowLeft />
+              Back to Explore page
+            </Button>)
+        }
+        {children}
+      </div>
     </div>
   )
 }
