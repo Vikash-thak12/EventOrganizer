2655f81be3bca6a82cfaeeb87fcff7508dc284a0
Vikash Thakur <thakvikash622@gmail.com>
2025-12-29 13:15:04 +0530

    Created: Custom-hook for User's onBoarding

---
+++ hooks/use-onboarding.ts
@@ -0,0 +1,56 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import { api } from "../convex/_generated/api";
+import { useConvexQuery } from "./use-convex-query";
+import { usePathname, useRouter } from "next/navigation";
+
+const ATTENDEE_PAGES = ["/explore", "/events", "/my-tickets", "/profile"];
+
+export function useBoarding() {
+
+    const [showOnboarding, setShowOnboarding] = useState(false);
+    const pathname = usePathname();
+    const router = useRouter();
+
+
+
+
+    const { data: currentUser, isLoading } = useConvexQuery(api.users.getCurrentUser);
+
+    useEffect(() => {
+        if (isLoading || !currentUser.hasCompletedOnboarding) {
+            return;
+        }
+
+        if (!currentUser.hasCompletedOnboarding) {
+            const requiresBoarding = ATTENDEE_PAGES.some((page) => {
+                pathname.startsWith(page);
+            })
+
+            if (requiresBoarding) {
+                // eslint-disable-next-line react-hooks/set-state-in-effect
+                setShowOnboarding(true);
+            }
+        }
+    }, [currentUser, isLoading, pathname]); 
+
+
+    const handleOnboardingComplete = () => {
+        setShowOnboarding(true); 
+        router.refresh(); 
+    }
+
+    const handleOnboardingSkip = () => {
+        setShowOnboarding(false); 
+        router.push("/")
+    }
+
+      return {
+    showOnboarding,
+    setShowOnboarding,
+    handleOnboardingComplete,
+    handleOnboardingSkip,
+    needsOnboarding: currentUser && !currentUser.hasCompletedOnboarding,
+  };
+}
